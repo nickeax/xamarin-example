4dd2d07a925d921896d4b70f1f52aa6d4bb24627
Nick Fletcher <nick.fletcher@iria.com.au>
2021-06-03 17:09:04 +1000

    Remove hyphen from project names.

---
+++ xamarin-example/xamarin-example/Views/SignInPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace xamarinExample.Views
+{
+    public class SignInPage : ContentView
+    {
+        public SignInPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
